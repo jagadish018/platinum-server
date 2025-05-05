54c188de5fd9cf85dbf3a86a9492d38217391890
jagadishck018 <jagadishck.018@gmail.com>
2025-05-05 11:02:06 +0530

    Refactor environment utility exports and update CORS origin to use webClientUrl

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,26 @@ import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import { authenticationsRoute } from './routes/authentications';
 import { cors } from 'hono/cors';
+import { webClientUrl } from './utils/environment';
 
 
 const allRoutes = new Hono();
 
 allRoutes.use(
   cors({
-    origin: '*',
+    origin: webClientUrl,
     allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowHeaders: ['Content-Type', 'Authorization'],
     exposeHeaders: ['Content-Length'],
     credentials: true,
     maxAge: 600,
-  })
-)
+  }),
+);
 allRoutes.use('*', logger());
 allRoutes.route('/authentication', authenticationsRoute);
 
 allRoutes.get('/', (c) => c.text('Hello Hono!'));
 
 serve(allRoutes, ({ port }) => {
-  console.log(`Localhost: ${port}`);
+  console.log(`http//:localhost:${port}`);
 });
