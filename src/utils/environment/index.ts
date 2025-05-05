3ce463fc03980b1b9fcb23d7f3cd63481be3d0aa
jagadishck018 <jagadishck.018@gmail.com>
2025-05-05 10:55:27 +0530

    Add betterAuthSecret and WebClientUrl exports to environment utility

--- src/utils/environment/index.ts
+++ src/utils/environment/index.ts
@@ -1,1 +1,5 @@
-export const serverUrl = process.env.SERVER_URL !;
+export const serverUrl = process.env.SERVER_URL!;
+
+export const betterAuthSceret = process.env.BETTER_AUTH_SECRET!;
+
+export const WebClientUrl = process.env.WEB_CLIENT_URL!;
