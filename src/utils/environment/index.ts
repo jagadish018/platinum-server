63a080c7e2caeb026f2b95428578c8b49868567a
jagadishck018 <jagadishck.018@gmail.com>
2025-05-06 10:33:29 +0530

    Fix typo in betterAuthSecret variable name in environment utility

--- src/utils/environment/index.ts
+++ src/utils/environment/index.ts
@@ -1,5 +1,5 @@
 export const serverUrl = process.env.SERVER_URL!;
 
-export const betterAuthSceret = process.env.BETTER_AUTH_SECRET!;
+export const betterAuthSecret = process.env.BETTER_AUTH_SECRET!;
 
 export const webClientUrl = process.env.WEB_CLIENT_URL!;
