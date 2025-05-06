05784f9f968db793d521c46aae26845bfbdde4c3
jagadishck018 <jagadishck.018@gmail.com>
2025-05-06 10:50:17 +0530

    Add cookie cache configuration to session settings in betterAuthClient

--- src/integration/better-auth/index.ts
+++ src/integration/better-auth/index.ts
@@ -16,6 +16,10 @@ export const betterAuthClient = betterAuth({
   },
   session: {
     modelName: "Session",
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60 ,
+    }
   },
   account: {
     modelName: "Account",
