87da0195a7677deb7b482c58c9aa7af89c2925a5
jagadishck018 <jagadishck.018@gmail.com>
2025-05-07 11:07:40 +0530

    Add user route with GET and PATCH endpoints for user data retrieval and updates

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { logger } from 'hono/logger';
 import { authenticationsRoute } from './routes/authentications';
 import { cors } from 'hono/cors';
 import { webClientUrl } from './utils/environment';
+import { postsRoute } from './routes/posts';
 
 
 const allRoutes = new Hono();
@@ -20,8 +21,8 @@ allRoutes.use(
 );
 allRoutes.use('*', logger());
 allRoutes.route("/authentication", authenticationsRoute);
-
+allRoutes.route("/posts", postsRoute);
 
 serve(allRoutes, ({ port }) => {
-  console.log(`http//:localhost:${port}`);
+  console.log(`Running at http//:localhost:${port}`);
 });
---
+++ src/routes/user/index.ts
@@ -0,0 +1,51 @@
+import { HTTPException } from "hono/http-exception";
+import { createSecureRoute } from "../middleware/session-middleware";
+import { prismaClient } from "../../integration/prisma";
+
+export const userRoute = createSecureRoute();
+userRoute.get("/me", async (context) => {
+  const user = context.get("user");
+
+  const getUser = await prismaClient.user.findUnique({
+    where: {
+      id: user.id,
+    },
+    include: {
+      posts: {
+        orderBy: {
+          createdAt: "desc", // Sort posts by creation date in descending order
+        },
+      },
+    },
+  });
+
+  if (!getUser) {
+    throw new HTTPException(404, {
+      message: "User not found",
+    });
+  }
+
+  return context.json({
+    user: getUser,
+  });
+});
+
+
+userRoute.patch("/", async (context) => {
+    const user = context.get("user");
+    const data = await context.req.json();
+    const updateUser = await prismaClient.user.update({
+        where: {
+            id: user.id
+        },
+        data: {
+            name: data.name,
+            about: data.about,
+            
+        }
+    });
+    return context.json({
+        user: updateUser
+    });
+});
+
